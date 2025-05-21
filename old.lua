8e5da6d7c89dc04f03d1919c297643a790731470
90opp <121714114+90opp@users.noreply.github.com>
2025-05-21 15:01:23 +0300

    Update old.lua

--- old.lua
+++ old.lua
@@ -1,95 +1,73 @@
+local HttpService = game:GetService("HttpService")
 local TeleportService = game:GetService("TeleportService")
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
--- твой placeId
 local placeId = 126884695634066
+local jobIds = {}
 
--- список jobId
-local jobids = {
-	"4d584a32-ff7e-4bca-b8e0-2bc9687b1c6a", "2c73109b-6cbf-4e82-b0c0-3f1188d3a775",
-	"16a5bce7-dacb-4b7d-9b7d-702216af9294", "91f43c4e-b0b1-4b72-8c0c-b975d55cd6d6",
-	"97727050-d015-4904-875c-24b4fb211c8a", "5d8b115d-67b7-4035-98e0-0a297b2a099c",
-	"73c19386-550d-4413-a2a7-f8299cbf5e48", "1f02bf30-bd07-46c8-a49d-70c5d44e8a02",
-	"646529d7-8a41-4a43-856d-996c6cc4957e", "8121fa73-e49e-4a03-941f-3ec922f71ddc",
-	"a311113b-6418-4fc1-87b3-2972ef316113", "78765ba1-ec40-4e6a-885b-9552714f391e",
-	"cb038504-24f8-4797-b50c-3a8f7c43c64c", "4b43698f-aa52-464f-b9d5-c52a4e779dea",
-	"a3ae7c9d-7c58-4a28-a078-9178644b1758", "869be9af-59c7-4a7e-a2fb-e1c1acb061b7",
-	"931e755d-c7c8-4f48-972f-048ddee6baff", "cc60c358-caf7-4a9a-91c4-253d4b41bd6c",
-	"d7f2df96-4ee7-496d-9daf-9d9298038d06", "318216fe-2549-45a4-b488-e51ab31c496b",
-	"27fb0499-dad1-4805-bbcc-cfe557549ee2", "209d73cf-1d6d-4c74-9142-2dd6203c18cb",
-	"7c3518b5-337e-41e9-8f70-f8824b658e25", "fba68cde-207b-4735-b3b9-66c8fd27374f",
-	"2945c7b6-564a-40d8-a233-c14f595cae46", "9ea81543-7281-4f33-b1b3-a2284287b589",
-	"6a8c8c05-8c84-4dbb-a889-21477938e03c", "4d9f9ea2-2fa8-4a80-b642-ea09babb4e00",
-	"e1bd4214-d6c0-45d5-8877-d54f0ad23bf3", "067f8b25-c462-469a-86b0-2bb65e7a8d5d",
-	"3ee8c741-6eed-44ab-a9dc-da0870923ff0", "e39d69f2-1bdd-4385-a068-740597ce0fcc",
-	"e8234da7-5c53-4008-aa36-5163b28ed96d", "f128295e-d0a2-4a41-ab25-6f8ff22e4ce7",
-	"55691982-d532-48f6-a183-f8e667fcc64e", "2a4c2857-f95a-429d-8971-5906ad341465",
-	"c42d2902-252c-4d3e-a78a-a117cf7d85c2", "744def34-9106-47ca-a7e7-4cbf260a9ad6",
-	"57e7f46b-e9d3-4dc3-be65-fb40c8153e22", "023a0682-5dc4-4964-b30d-a722233316b4",
-	"e84f9994-409e-4c0e-8e78-8db6ed2b4b81", "ec16b73f-331c-409b-a94e-a89ab8af8dfd",
-	"93931bbe-bf12-49bf-9521-3e8edc6e041e", "6c1493ef-d5e2-4b38-a272-ff35dbe65245",
-	"7f2e7d19-e518-4f0d-972d-2be65d506a04", "30903540-da51-47d0-961b-38eeea8d1a69",
-	"82bd5c47-0bae-4892-89e1-6c7dc3fa4092", "5a5f68a3-103a-4b41-bcad-aab887b54ba8",
-	"1c0e2436-aa2a-461f-bb9a-b7f090ac52a0", "65e66885-2c29-4d48-918c-fd93f568a571",
-	"d187f664-c521-442d-ae47-491806da43aa", "1d5ac5d2-905f-4d23-a6a4-39c973625a44",
-	"f8a1019c-b6c6-425d-aebb-4704c876be4b", "66fcd226-1201-4a2c-87ae-959dbc4cd5d8",
-	"5931a958-8ccf-4816-a7a9-92a87b0b49aa", "896b6bfb-6f3c-416e-8654-04805902a9a7",
-	"7a6ec2c6-5bd5-4478-b6ae-d4f5c9b7e657", "43db38ae-c551-41cb-8ef4-0d7d13485880",
-	"bcc361c5-1346-472b-8e38-2d0e395adbcc", "bfab4fb3-4efb-4825-84ad-5b7682261bf3",
-	"d2dfb802-b30b-41dd-86b1-e85d5bab9c98", "72e2293b-eb93-4ede-9007-b76fa7b8d8e5",
-	"0dfe6962-f056-445b-9038-ac2faa384690", "fc0cb970-ba85-4936-b5f9-09aff8d74341",
-	"2eec06be-b2a0-4541-a129-d08975343f47", "4bc4fd6a-0be9-4aac-a42b-2ef7d49bc401",
-	"ce344707-8ff2-437c-a8f9-1d850ff03fbb", "65859db2-1742-403d-8ad1-342745347b9a",
-	"d2d4d4ec-ad7d-4469-be7b-a155f039d20c", "2e07ef27-1d33-4a36-a36a-77cbfb7f7c2f",
-	"f9112f4e-652b-4acf-b6a4-a6f8dcf0b335", "702112db-4cc2-429f-bd0b-220eaf8175d0",
-	"3cd56dac-8af4-4db4-845c-5c0180e27f35", "6af16cf1-6760-4e3e-8069-635cbeeadee3",
-	"eea87841-850e-4543-aca6-68881c18119c", "095e78b6-2583-4664-96d2-a12a2c3b6499",
-	"ea807338-9cf3-4cd6-a8c5-d2c53587a282"
-}
+local url = "https://raw.githubusercontent.com/90opp/oldservertest/refs/heads/main/servers.txt"
 
--- перемешивание списка
-local function shuffle(tbl)
-	for i = #tbl, 2, -1 do
-		local j = math.random(i)
-		tbl[i], tbl[j] = tbl[j], tbl[i]
-	end
-	return tbl
+local function fetchJobIds()
+    local success, response = pcall(function()
+        return HttpService:GetAsync(url)
+    end)
+    if success then
+        local lines = {}
+        for line in response:gmatch("[^\r\n]+") do
+            line = line:match("^%s*(.-)%s*$") -- убираем пробелы по краям
+            if #line > 0 then
+                table.insert(lines, line)
+            end
+        end
+        return lines
+    else
+        warn("Ошибка загрузки jobId: " .. tostring(response))
+        return nil
+    end
 end
 
--- флаг, чтобы не спамить
 local teleporting = false
 
--- обработка ошибки
 TeleportService.TeleportInitFailed:Connect(function(player, result)
-    warn("Teleport failed: " .. tostring(result))
+    warn("TeleportInitFailed: " .. tostring(result))
     teleporting = false
 end)
 
--- цикл телепорта
+local function tryTeleport(jobId)
+    local success, err = pcall(function()
+        TeleportService:TeleportToPlaceInstance(placeId, jobId, player)
+    end)
+    if not success then
+        warn("Ошибка при попытке телепорта: " .. tostring(err))
+        return false
+    end
+    return true
+end
+
 task.spawn(function()
+    jobIds = fetchJobIds()
+    if not jobIds or #jobIds == 0 then
+        warn("Список jobId пуст или не загружен.")
+        return
+    end
+
     while true do
         if not teleporting then
             teleporting = true
 
-            local shuffledList = shuffle(jobIds)
-            for _, jobId in ipairs(shuffledList) do
-                print("Пробуем сервер: " .. jobId)
-                local success, err = pcall(function()
-                    TeleportService:TeleportToPlaceInstance(placeId, jobId, player)
-                end)
-
-                if not success then
-                    warn("Ошибка телепорта: " .. tostring(err))
-                    task.wait(10)
-                else
-                    print("Телепорт инициализирован, ждём переход...")
+            for i, jobId in ipairs(jobIds) do
+                print("Пытаемся телепортироваться на сервер:", jobId)
+                local success = tryTeleport(jobId)
+                if success then
+                    print("Телепорт инициализирован, ждем переход...")
                     break
+                else
+                    print("Неудачный телепорт, ждем 10 сек и пробуем следующий сервер.")
+                    task.wait(10)
                 end
             end
 
-            -- подождем, если ничего не сработало
-            task.wait(10)
             teleporting = false
         end
         task.wait(5)
